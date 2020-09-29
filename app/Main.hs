4a538dcef7aa70a7bb5c46b3795b2a0032832c9f
MichaelObvious <michele.palese42@outlook.it>
2020-09-29 17:53:44 +0200

    producing music from hsv

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,12 @@
 {-# LANGUAGE NumericUnderscores #-}
 
+{-
+TODO:
+  bpm
+  attack, decay
+  hue to note
+-}
+
 module Main where
 
 import           Lib
@@ -9,58 +16,89 @@ import qualified Data.ByteString.Builder as B
 import qualified Data.ByteString.Lazy as B
 import           Data.Fixed
 import           Data.Foldable
+import           Data.List
 import qualified Data.Vector.Storable as V
 import           GHC.Word
 import           System.Environment
 import           System.Process
 import           Text.Printf
 
+type RGB = (Float, Float, Float)
+type HSV = (Float, Float, Float)
 type Hz = Float
 type Seconds = Float
+type Pulse = Float
 
 sampleRate :: Float
 sampleRate = 48_000
 
 volume :: Float
-volume = 0.5
+volume = 0.75
 
-imageData2rgb :: [Word8] -> [(Float, Float, Float)]
+pitchStandard :: Hz
+pitchStandard = 440.0
+
+imageData2rgb :: [Word8] -> [RGB]
 imageData2rgb [] = []
 imageData2rgb (r:g:b:xs) = [((fromIntegral r) / 255.0, (fromIntegral g) / 255.0, (fromIntegral b) / 255.0)] ++ imageData2rgb xs
 
-imageToPixels :: DynamicImage -> [(Float, Float, Float)]
+imageToPixels :: DynamicImage -> [RGB]
 imageToPixels image = imageData2rgb $ V.toList $ imageData $ convertRGB8 image
 
-rgb2hue :: (Float, Float, Float) -> Float
-rgb2hue (r, g, b)
-  | m_big == m_little = 0
-  | m_big == r = ((g - b) / c `mod'` 6.0) / 6.0
-  | m_big == g = ((b - r) / c + 2.0) / 6.0
-  | m_big == b = ((r - g) / c + 4.0) / 6.0
+rgb2hsv :: RGB -> HSV
+rgb2hsv (r, g, b)
+  | m_big == m_little = (0.0, s, v)
+  | m_big == r = (((g - b) / c `mod'` 6.0) / 6.0, s, v)
+  | m_big == g = (((b - r) / c + 2.0) / 6.0, s, v)
+  | m_big == b = (((r - g) / c + 4.0) / 6.0, s, v)
    -- should never happen
   | otherwise = undefined
-  
+
   where
     infinity = read "Infinity"
     m_big = foldl max 0 [r, g, b]
     m_little = foldl min infinity [r, g, b]
     c = m_big - m_little
+    s = if m_big == 0 then 0 else c
+    v = m_big
 
-hue2hz :: Float -> Hz
-hue2hz hue = (hue * 389 + 400) * 10 ^ 12
+hsv2hz :: HSV -> Hz
+hsv2hz (h, _, v) = hv^2 * (max_hz - min_hz) + min_hz
+  where
+    hv = h * v
+    max_hz = 15_000
+    min_hz = 30
 
-wave :: Hz -> Seconds -> [Float]
-wave freq duration = map ((* volume) . sin . (* step)) [0.0 .. sampleRate * duration]
+hsv2note :: HSV -> Hz
+hsv2note (h, _, v) = note semitone_d
+  where
+    hv = h * v
+    semitone_d = fromIntegral $ round (hv * 88.0 - 39.0)
+
+    note :: Float -> Hz
+    note n = pitchStandard * (2 ** (1.0 / 12.0)) ** n
+
+hsv2volume :: HSV -> Float
+hsv2volume (_, s, _) = s * volume
+
+image2wave :: (HSV -> Hz) -> DynamicImage -> [Pulse]
+image2wave hsvtofreq image = concat $ map (hsv2wave . head) $ group hsvs
+  where 
+    hsvs = map rgb2hsv $ imageToPixels image
+    hsv2wave hsv = wave (hsvtofreq hsv) (hsv2volume hsv) 1.0 
+
+wave :: Hz -> Float -> Seconds -> [Pulse]
+wave freq vol duration = map ((* vol) . sin . (* step)) [0.0 .. sampleRate * duration]
   where
     step = (freq * 2 * pi) / sampleRate
 
-save :: [Float] -> FilePath -> IO ()
-save samples fileName = B.writeFile fileName $ B.toLazyByteString $ fold $ map B.floatLE samples
+save :: FilePath -> [Pulse] -> IO ()
+save fileName samples = B.writeFile fileName $ B.toLazyByteString $ fold $ map B.floatLE samples
 
 -- Only for testing in the REPL
 play :: [Float] -> IO ()
 play samples = do
-  save samples filePath
+  save filePath samples
   _ <- runCommand $ printf "ffplay -showmode 1 -f f32le -ar %f %s" sampleRate filePath
   return ()
   where filePath = "test.bin"
@@ -71,5 +109,5 @@ main = do
   eimg <- readImage path
   case eimg of
     Left _ -> error "Could not load image"
-    Right image -> putStrLn $ show $ imageToPixels image
+    Right image -> play $ image2wave hsv2note image
     _ -> error "Unsupported pixel format"
